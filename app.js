9d98e55d095880ada04f39ca5c427cd74bf57989
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-10-24 12:23:30 -0400

    set up a conditional for setting status codes

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Node.js HTTP Server</h1>');
-  res.end();
+    const url = req.url;
+    // Home Page
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    } 
+    // About Page
+    else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>About Us</h1>');
+        res.end();
+    } 
+    // Contact Page
+    else if (url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Contact Us</h1>');
+        res.end();
+    } 
+    // 404 Page -  if nothing matches
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('<h1>Page Not Found</h1>');
+        res.end();
+    }
+  
 });
 
 server.listen(5000);
