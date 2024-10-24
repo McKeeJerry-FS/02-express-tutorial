6643d3055323aba977f615c8261dc3ac173f075f
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-10-24 15:10:22 -0400

    working on queries and params for API requests

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// setup static and middleware
-app.use(express.static('./public'));
+const {products} = require('./data');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+})
+
+// Setting up the GET request for the products API (all products)
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name, image};
+    });
+    res.json(newProducts);
 });
 
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>');    
-});
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req.params);
+    // console.log(req.params.productID);
+    const { productID } = req.params;
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+    );
+    if(!singleProduct){
+        return res.status(404).send('Product does not exist'); 
+    } 
+    return res.status(200).json(singleProduct);
+})
+
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000....');
-});
+    console.log('Server is running on port 5000....');
+})
