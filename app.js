007badb045f0877b52a6deb2755eea58c2eecae1
Jerry McKee Jr <jmckeejr@safehomesecurityinc.com>
2024-10-25 13:08:08 -0400

    fix a few errors; added code for all HTTP Methods

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 let { people } = require('./data');
 
+// Static Assets
+app.use(express.static('./methods-public'));
+
+// Parse Form Data
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON
+app.use(express.json());
+
+
 // Get all People
 // ./api/people
 app.get('/api/people', (req, res) => {
@@ -20,6 +29,26 @@ app.get('/api/people/:id', (req, res) => {
     }
     res.status(200).json({ success: true, data: person });
 });
+
+
+// POST - Insert Data
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    res.status(401).send('Please Provide Credentials');
+});
+
+// POST - Insert Data
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'Please Provide Name Value' });
+    }
+    res.status(201).json({ success: true, person: name });
+    
+})
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000....');
